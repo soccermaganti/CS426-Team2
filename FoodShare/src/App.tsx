c5674f828204c636c01adf106d3c93b745ae054e
Sriram Maganti <smaganti@umass.edu>
2025-03-29 19:53:44 -0400

    Fixed the UserDashboard UI colors to match the correct styling we set in the UI/UX document. Needs review and changes as we go but for now this should be good for the milestone

--- FoodShare/src/App.tsx
+++ FoodShare/src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
       <Router>
-        <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col min-h-screen bg-white">
           <main className="flex-grow w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
             <Routes>
               <Route path="/" element={<Login />} />
--- FoodShare/src/pages/dashboard.tsx
+++ FoodShare/src/pages/dashboard.tsx
@@ -3,7 +3,7 @@ import UserDashboard from '../components/UserDashboard'
 const DashBoard: React.FC = () => {
     return (
         <div>
-            <section className="bg-black rounded-lg shadow-md p-6 sticky top-6">
+            <section className="bg-gray-100 rounded-lg shadow-md p-6 sticky top-6">
             {/* <section> */}
                 <UserDashboard />
             </section>
