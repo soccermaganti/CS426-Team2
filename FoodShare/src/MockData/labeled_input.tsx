47640b956a1c826673e5baa8ea0838a57b717a60
Jeremy Ngooi <jngooi@umass.edu>
2025-04-03 12:58:40 -0400

    Made styling consistent with labelled inputs

--- FoodShare/src/MockData/labeled_input.tsx
+++ FoodShare/src/MockData/labeled_input.tsx
@@ -5,15 +5,17 @@ const LabeledInput = ({ label, type, placeholder, value, setValue }: {
     value: string;
     setValue: (val: string) => void;
 }) => (
-    <label>
-        {label}: 
+    <div className="flex flex-col space-y-1">
+        <label className="text-green-700 font-medium">{label}</label> 
         <input 
             type={type} 
             placeholder={placeholder} 
             value={value} 
             onChange={(e) => setValue(e.target.value)} 
+            className="w-full px-4 py-2 border rounded-lg bg-green-50 focus:outline-none focus:ring-2 focus:ring-green-500"
         />
-    </label>
+    </div>
+    
 );
 
 export { LabeledInput }
